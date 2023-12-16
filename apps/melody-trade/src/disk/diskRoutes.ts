403b320b23f6efdd3ca6e6b40bd110592fe42ee5
neo-kjb <anaszawahreh.dev@gmail.com>
2023-12-16 06:30:06 +0300

    Adding deleteDisk route

--- apps/melody-trade/src/disk/diskRoutes.ts
+++ apps/melody-trade/src/disk/diskRoutes.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { createDisk, editDisk, getDiskDetails, getDisks } from "./diskController";
+import { createDisk, deleteDisk, editDisk, getDiskDetails, getDisks } from "./diskController";
 import { validateDiskInputMiddleware } from "./middleware";
 import { isAuth } from "../user/middleware";
 const router = express.Router()
@@ -8,5 +8,5 @@ router.get('/',getDisks)
 router.post('/add',isAuth,validateDiskInputMiddleware,createDisk)
 router.get('/:id',getDiskDetails)
 router.put('/:id/edit',isAuth,validateDiskInputMiddleware,editDisk)
-router.delete('/:id')
+router.delete('/:id',isAuth,deleteDisk)
 export default router
