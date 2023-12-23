e2478bdce1b724dc736376bb8cff98c5d5be851a
neo-kjb <anaszawahreh.dev@gmail.com>
2023-12-23 19:19:05 +0300

    Testing getPendingSwapsForUser Route

--- apps/melody-trade/src/swap/swapRoutes.spec.ts
+++ apps/melody-trade/src/swap/swapRoutes.spec.ts
@@ -5,74 +5,74 @@ import { DiskService } from '../disk/diskService';
 import { SwapService } from './swapService';
 
 describe('Swap Routes', () => {
-  describe('Post/swaps Create Swap', () => {
-    let user1AccessToken
-    let user2AccessToken
-    let disk1
-    let disk2
-    let disk3
-    beforeAll(async () => {
-      await SwapService.deleteAllSwaps()
-      await UserService.deleteAllUsers()
-      await DiskService.deleteAllDisks()
-      await request(app)
-        .post('/auth/signup')
-        .send({
-          username: 'username',
-          email: 'username@email.com',
-          password: 'password',
-        });
-      await request(app)
-        .post('/auth/signup')
-        .send({
-          username: 'username2',
-          email: 'username2@email.com',
-          password: 'password',
-        });
-      const loginResponse1 = await request(app).post('/auth/login').send({
+  let user1AccessToken
+  let user2AccessToken
+  let disk1
+  let disk2
+  let disk3
+  beforeAll(async () => {
+    await SwapService.deleteAllSwaps()
+    await UserService.deleteAllUsers()
+    await DiskService.deleteAllDisks()
+    await request(app)
+      .post('/auth/signup')
+      .send({
+        username: 'username',
         email: 'username@email.com',
         password: 'password',
       });
-      const loginResponse2 = await request(app).post('/auth/login').send({
+    await request(app)
+      .post('/auth/signup')
+      .send({
+        username: 'username2',
         email: 'username2@email.com',
         password: 'password',
       });
-      user1AccessToken = loginResponse1.body.accessToken;
-      user2AccessToken = loginResponse2.body.accessToken;
-      const diskData1 = {
-        name: 'disk1',
-        description: 'description1',
-        location: 'location1',
-        imageURL: 'https://url1.com',
-        userId: loginResponse1.body.user.id
-      };
+    const loginResponse1 = await request(app).post('/auth/login').send({
+      email: 'username@email.com',
+      password: 'password',
+    });
+    const loginResponse2 = await request(app).post('/auth/login').send({
+      email: 'username2@email.com',
+      password: 'password',
+    });
+    user1AccessToken = loginResponse1.body.accessToken;
+    user2AccessToken = loginResponse2.body.accessToken;
+    const diskData1 = {
+      name: 'disk1',
+      description: 'description1',
+      location: 'location1',
+      imageURL: 'https://url1.com',
+      userId: loginResponse1.body.user.id
+    };
 
-      const diskData2 = {
-        name: 'disk2',
-        description: 'description2',
-        location: 'location2',
-        imageURL: 'https://url2.com',
-        userId: loginResponse2.body.user.id
-      };
+    const diskData2 = {
+      name: 'disk2',
+      description: 'description2',
+      location: 'location2',
+      imageURL: 'https://url2.com',
+      userId: loginResponse2.body.user.id
+    };
 
-      const diskData3 = {
-        name: 'disk3',
-        description: 'description3',
-        location: 'location3',
-        imageURL: 'https://url3.com',
-        userId: loginResponse1.body.user.id
-      };
+    const diskData3 = {
+      name: 'disk3',
+      description: 'description3',
+      location: 'location3',
+      imageURL: 'https://url3.com',
+      userId: loginResponse1.body.user.id
+    };
 
-      disk1 = await DiskService.createDisk(diskData1);
-      disk2 = await DiskService.createDisk(diskData2);
-      disk3 = await DiskService.createDisk(diskData3);
+    disk1 = await DiskService.createDisk(diskData1);
+    disk2 = await DiskService.createDisk(diskData2);
+    disk3 = await DiskService.createDisk(diskData3);
 
-    })
-    afterAll(async () => {
-      await SwapService.deleteAllSwaps()
-      await UserService.deleteAllUsers()
-      await DiskService.deleteAllDisks()
-    })
+  })
+  afterAll(async () => {
+    await SwapService.deleteAllSwaps()
+    await UserService.deleteAllUsers()
+    await DiskService.deleteAllDisks()
+  })
+  describe('Post/swaps Create Swap', () => {
     test('should return 201 status code , a message and swap details', async () => {
       const swapResponse = await request(app)
         .post('/swaps')
@@ -140,10 +140,38 @@ describe('Swap Routes', () => {
       expect(swapResponse.statusCode).toBe(400)
 
     })
+  })
 
+
+  describe('GET / get pending swaps', () => {
+
+    test('should return 200 status code and a count and pending swaps array', async () => {
+      await request(app)
+        .post('/swaps')
+        .set('Authorization', `Bearer ${user1AccessToken}`)
+        .send({
+          sentItemId: disk1.id,
+          receivedItemId: disk2.id
+        })
+
+
+      const getPendingSwapsResponse = await request(app)
+        .get('/swaps')
+        .set('Authorization', `Bearer ${user1AccessToken}`)
+      console.log(getPendingSwapsResponse.body, getPendingSwapsResponse.statusCode);
+
+      expect(getPendingSwapsResponse.statusCode).toBe(200)
+      expect(getPendingSwapsResponse.body.count).toBe(1)
+      getPendingSwapsResponse.body.data.map(swap => {
+        expect(swap.status).toBe('pending')
+        expect(swap.sentItemId).toBe(disk1.id)
+        expect(swap.receivedItemId).toBe(disk2.id)
+      })
+    })
 
 
 
   })
+
 
 })
