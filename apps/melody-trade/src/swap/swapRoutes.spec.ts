ac0cda4b2ba35f7300d8716bf7acad0ce1956d24
neo-kjb <anaszawahreh.dev@gmail.com>
2023-12-26 19:10:38 +0300

    Testing acceptSwap route

--- apps/melody-trade/src/swap/swapRoutes.spec.ts
+++ apps/melody-trade/src/swap/swapRoutes.spec.ts
@@ -7,6 +7,8 @@ import { SwapService } from './swapService';
 describe('Swap Routes', () => {
   let user1AccessToken
   let user2AccessToken
+  let user1Id
+  let user2Id
   let disk1
   let disk2
   let disk3
@@ -38,12 +40,14 @@ describe('Swap Routes', () => {
     });
     user1AccessToken = loginResponse1.body.accessToken;
     user2AccessToken = loginResponse2.body.accessToken;
+    user1Id = loginResponse1.body.user.id
+    user2Id = loginResponse2.body.user.id
     const diskData1 = {
       name: 'disk1',
       description: 'description1',
       location: 'location1',
       imageURL: 'https://url1.com',
-      userId: loginResponse1.body.user.id
+      userId: user1Id
     };
 
     const diskData2 = {
@@ -51,7 +55,7 @@ describe('Swap Routes', () => {
       description: 'description2',
       location: 'location2',
       imageURL: 'https://url2.com',
-      userId: loginResponse2.body.user.id
+      userId: user2Id
     };
 
     const diskData3 = {
@@ -259,6 +263,69 @@ describe('Swap Routes', () => {
 
 
   })
+
+  describe('PUT /:swapId/accept accept swap route', () => {
+    beforeEach(async () => {
+      await SwapService.deleteAllSwaps()
+    })
+    test('should return 200 status code and a swap status of -accepted- and the receiver only should be the acceptor', async () => {
+      const createSwapResponse = await request(app)
+        .post('/swaps')
+        .set('Authorization', `Bearer ${user1AccessToken}`)
+        .send({
+          sentItemId: disk3.id,
+          receivedItemId: disk2.id
+        })
+
+      const acceptSwapResponse = await request(app)
+        .put(`/swaps/${createSwapResponse.body.swap.id}/accept`)
+        .set('Authorization', `Bearer ${user2AccessToken}`)
+
+      expect(acceptSwapResponse.statusCode).toBe(200)
+      expect(acceptSwapResponse.body.updatedSwap.id).toBe(createSwapResponse.body.swap.id)
+      expect(acceptSwapResponse.body.updatedSwap.status).toBe('accepted')
+      expect(acceptSwapResponse.body.updatedSwap.receiverId).toBe(user2Id)
+    })
+
+    test('should return 400 status code for invalid swap ID', async () => {
+      const acceptSwapResponse = await request(app)
+        .put('/swaps/invalidStringId/accept')
+        .set('Authorization', `Bearer ${user2AccessToken}`)
+      expect(acceptSwapResponse.statusCode).toBe(400)
+      expect(acceptSwapResponse.body.message).toBe('Invalid swap ID')
+    })
+    test('should return 404 status code for swap not found', async () => {
+      const acceptSwapResponse = await request(app)
+        .put('/swaps/11/accept')
+        .set('Authorization', `Bearer ${user2AccessToken}`)
+      expect(acceptSwapResponse.statusCode).toBe(404)
+      expect(acceptSwapResponse.body.message).toBe('Swap not found')
+    })
+    test('should return 401 status code for unauthorized user', async () => {
+      const createSwapResponse = await request(app)
+        .post('/swaps')
+        .set('Authorization', `Bearer ${user1AccessToken}`)
+        .send({
+          sentItemId: disk1.id,
+          receivedItemId: disk2.id
+        })
+
+      const acceptSwapResponse = await request(app)
+        .put(`/swaps/${createSwapResponse.body.swap.id}/accept`)
+        .set('Authorization', `Bearer ${user1AccessToken}`)
+      console.log(acceptSwapResponse.statusCode, acceptSwapResponse.body);
+
+      expect(acceptSwapResponse.statusCode).toBe(401)
+      expect(acceptSwapResponse.body.message).toBe('Unauthorized to accept the swap')
+
+    })
+
+
+
+
+  })
+
+
 
 
 })
