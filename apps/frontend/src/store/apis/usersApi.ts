dad0974879ced5bc25e4ac9fb7e8249c7c3fab90
neo-kjb <anaszawahreh.dev@gmail.com>
2023-12-29 06:28:45 +0300

    Creating usersApi

---
+++ apps/frontend/src/store/apis/usersApi.ts
@@ -0,0 +1,67 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { getAuthToken } from "../../utils/getAuthToken";
+
+const usersApi = createApi({
+  reducerPath: "users",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:3333/users",
+  }),
+  endpoints(builder) {
+    return {
+      addUser: builder.mutation({
+        query: (user) => {
+          const { username, email, password } = user;
+          return {
+            url: "/signup",
+            method: "POST",
+            body: {
+              username,
+              email,
+              password,
+            },
+          };
+        },
+      }),
+      loginUser: builder.mutation({
+        query: (user) => {
+          const { email, password } = user;
+          return {
+            url: "/login",
+            method: "POST",
+            body: {
+              email,
+              password,
+            },
+          };
+        },
+      }),
+      getCurUser: builder.query({
+        query: () => {
+          return {
+            url: "/auth",
+            method: "GET",
+            headers: {
+              Authorization: "Bearer " + getAuthToken(),
+            },
+          };
+        },
+      }),
+      getUserDetails: builder.query({
+        query: (username) => {
+          return {
+            url: `/${username}`,
+            method: "GET",
+          };
+        },
+      }),
+    };
+  },
+});
+
+export { usersApi };
+export const {
+  useAddUserMutation,
+  useGetCurUserQuery,
+  useGetUserDetailsQuery,
+  useLoginUserMutation,
+} = usersApi;
