4c9a468c7b674b1f55fbb03b2f687b6c78d99f72
neo-kjb <anaszawahreh.dev@gmail.com>
2023-12-29 05:22:31 +0300

    Editing content path

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./apps/frontend/index.html",
+    "./apps/frontend/src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
