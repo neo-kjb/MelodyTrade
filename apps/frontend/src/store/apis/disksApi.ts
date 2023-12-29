54f248ca1a2bfabfa694b66f18ef7800d655c834
neo-kjb <anaszawahreh.dev@gmail.com>
2023-12-29 07:02:35 +0300

    Creating disksApi

---
+++ apps/frontend/src/store/apis/disksApi.ts
@@ -0,0 +1,80 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { getAuthToken } from '../../utils/getAuthToken';
+
+const disksApi = createApi({
+  reducerPath: 'disks',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'http://localhost:3333/disks',
+  }),
+  endpoints(builder) {
+    return {
+      getAllDisks: builder.query({
+        query: () => {
+          return {
+            url: '/',
+            method: 'GET',
+          };
+        },
+      }),
+      createDisk: builder.mutation({
+        query: (disk) => {
+          const { name, description, location, imageURL } = disk;
+          return {
+            url: '/add',
+            method: 'POST',
+            headers: {
+              Authorization: 'Bearer ' + getAuthToken(),
+            },
+            body: {
+              name,
+              description,
+              location,
+              imageURL,
+            },
+          };
+        },
+      }),
+      getDiskDetails: builder.query({
+        query: (diskId) => {
+          return {
+            url: `/${diskId}`,
+            method: 'GET',
+          };
+        },
+      }),
+      editDisk: builder.mutation({
+        query: (disk) => {
+          const { name, description, location, imageURL } = disk;
+          return {
+            url: `/${disk.id}/edit`,
+            method: 'PUT',
+            headers: {
+              Authorization: 'Bearer ' + getAuthToken(),
+            },
+            body: { name, description, location, imageURL },
+          };
+        },
+      }),
+      deleteDisk: builder.mutation({
+        query: (diskId) => {
+          return {
+            url: `/${diskId}`,
+            method: 'DELETE',
+            headers: {
+              Authorization: 'Bearer ' + getAuthToken(),
+            },
+          };
+        },
+      }),
+    };
+  },
+});
+
+export { disksApi };
+export const {
+  useGetAllDisksQuery,
+  useCreateDiskMutation,
+  useDeleteDiskMutation,
+  useEditDiskMutation,
+  useGetDiskDetailsQuery,
+} = disksApi;
--- apps/frontend/src/store/index.ts
+++ apps/frontend/src/store/index.ts
@@ -1,24 +1,34 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { setupListeners } from "@reduxjs/toolkit/query";
+import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
 
-import { usersApi } from "./apis/usersApi";
+import { usersApi } from './apis/usersApi';
+import { disksApi } from './apis/disksApi';
 
 export const store = configureStore({
   reducer: {
     [usersApi.reducerPath]: usersApi.reducer,
+    [disksApi.reducerPath]: disksApi.reducer,
   },
   middleware: (getDefaultMiddleware) => {
     return getDefaultMiddleware()
-      .concat(usersApi.middleware);
+      .concat(usersApi.middleware)
+      .concat(disksApi.middleware);
   },
 });
 
 setupListeners(store.dispatch);
-
 
 export {
   useAddUserMutation,
   useGetCurUserQuery,
   useGetUserDetailsQuery,
   useLoginUserMutation,
-} from "./apis/usersApi";
+} from './apis/usersApi';
+
+export {
+  useCreateDiskMutation,
+  useDeleteDiskMutation,
+  useEditDiskMutation,
+  useGetAllDisksQuery,
+  useGetDiskDetailsQuery,
+} from './apis/disksApi';
