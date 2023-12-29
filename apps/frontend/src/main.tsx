a9b46ff66e4ba1b3817bd4480f364b04dca5d0cc
neo-kjb <anaszawahreh.dev@gmail.com>
2023-12-29 06:29:16 +0300

    Providing store

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,16 +1,17 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 
 import App from './app';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <StrictMode>
-    <BrowserRouter>
+    <Provider store={store}>
       <App />
-    </BrowserRouter>
+    </Provider>
   </StrictMode>
 );
