c366cd7a2083a2b1b466e23652bdb7994fe24f45
neo-kjb <anaszawahreh.dev@gmail.com>
2023-12-30 07:17:33 +0300

    Adding cors

--- apps/melody-trade/src/app.ts
+++ apps/melody-trade/src/app.ts
@@ -1,14 +1,17 @@
 import express from 'express';
-import usersRoutes from './user/userRoutes'
-import disksRoutes from './disk/diskRoutes'
-import swapsRoutes from './swap/swapRoutes'
+import cors from 'cors';
+
+import usersRoutes from './user/userRoutes';
+import disksRoutes from './disk/diskRoutes';
+import swapsRoutes from './swap/swapRoutes';
+
 const app = express();
-app.use(express.json())
 
-app.use('/auth', usersRoutes)
-app.use('/disks', disksRoutes)
-app.use('/swaps', swapsRoutes)
+app.use(cors());
+app.use(express.json());
 
+app.use('/auth', usersRoutes);
+app.use('/disks', disksRoutes);
+app.use('/swaps', swapsRoutes);
 
-
-export default app
+export default app;
