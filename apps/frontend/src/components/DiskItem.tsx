ad4141a718d98b568060d1f4c5316acb5f4579d3
neo-kjb <anaszawahreh.dev@gmail.com>
2024-01-02 08:21:03 +0300

    Creating DiskIndex Page

---
+++ apps/frontend/src/components/DiskItem.tsx
@@ -0,0 +1,25 @@
+import { useNavigate } from 'react-router-dom';
+
+export default function DiskItem({ disk }) {
+  const navigate = useNavigate();
+
+  return (
+    <div className="relative bg-white p-4 rounded-lg shadow-md mb-4 md:mr-4 flex">
+      <img
+        src={disk.imageURL}
+        alt={disk.name}
+        className="w-1/3 object-cover rounded-lg mr-4"
+      />
+      <div className="flex-1">
+        <h2 className="text-lg font-medium text-gray-800 mb-2">{disk.name}</h2>
+        <p className="text-gray-600">{disk.description}</p>
+      </div>
+      <button
+        onClick={() => navigate(`/disks/${disk.id}`)}
+        className="absolute bottom-0 right-0 mr-4 mb-4 bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50"
+      >
+        {`View ${disk.name} disk`}
+      </button>
+    </div>
+  );
+}
--- apps/frontend/src/pages/disks/DisksIndexPage.tsx
+++ apps/frontend/src/pages/disks/DisksIndexPage.tsx
@@ -1,5 +1,27 @@
-import React from 'react';
+import { useGetAllDisksQuery } from '../../store';
+import DiskItem from '../../components/DiskItem';
+import Skeleton from '../../components/Skeleton';
+import { useSnackbar } from 'notistack';
 
 export default function DisksIndexPage() {
-  return <div>DisksIndexPage</div>;
+  const { enqueueSnackbar } = useSnackbar();
+  const { data, isLoading, isError } = useGetAllDisksQuery();
+
+  let content;
+  if (isLoading) {
+    content = <Skeleton className={'h-48 w-full'} times={3} />;
+  } else if (isError) {
+    enqueueSnackbar('Error Loading Disks, Please Refresh the Page...', {
+      variant: 'error',
+    });
+    content = <div>Error Loading Disks!</div>;
+  } else {
+    content = data.data.map((disk) => <DiskItem key={disk.id} disk={disk} />);
+  }
+  return (
+    <>
+      <h1 className="text-xl m-6">All Disks {`(${data?.count})`}</h1>
+      <div>{content}</div>
+    </>
+  );
 }
