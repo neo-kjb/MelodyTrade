dc34f772fe585b88940ba95406ee7fd7cad03bd0
neo-kjb <anaszawahreh.dev@gmail.com>
2024-01-03 18:07:50 +0300

    Adding getDisksByUsername route

--- apps/melody-trade/src/disk/diskRoutes.ts
+++ apps/melody-trade/src/disk/diskRoutes.ts
@@ -1,12 +1,20 @@
-import express from "express";
-import { createDisk, deleteDisk, editDisk, getDiskDetails, getDisks } from "./diskController";
-import { validateDiskInputMiddleware } from "./middleware";
-import { isAuth } from "../user/middleware";
-const router = express.Router()
+import express from 'express';
+import {
+  createDisk,
+  deleteDisk,
+  editDisk,
+  getDiskDetails,
+  getDisks,
+  getDisksByUser,
+} from './diskController';
+import { validateDiskInputMiddleware } from './middleware';
+import { isAuth } from '../user/middleware';
+const router = express.Router();
 
-router.get('/',getDisks)
-router.post('/add',isAuth,validateDiskInputMiddleware,createDisk)
-router.get('/:id',getDiskDetails)
-router.put('/:id/edit',isAuth,validateDiskInputMiddleware,editDisk)
-router.delete('/:id',isAuth,deleteDisk)
-export default router
+router.get('/', getDisks);
+router.get('/user/:username', getDisksByUser);
+router.post('/add', isAuth, validateDiskInputMiddleware, createDisk);
+router.get('/:id', getDiskDetails);
+router.put('/:id/edit', isAuth, validateDiskInputMiddleware, editDisk);
+router.delete('/:id', isAuth, deleteDisk);
+export default router;
