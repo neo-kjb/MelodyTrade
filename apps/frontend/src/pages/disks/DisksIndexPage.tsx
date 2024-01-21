7c1393fdd575d1d44c4a251c6c6e888819292693
neo-kjb <anaszawahreh.dev@gmail.com>
2024-01-21 18:09:42 +0300

    Adding Search feature

--- apps/frontend/src/pages/disks/DisksIndexPage.tsx
+++ apps/frontend/src/pages/disks/DisksIndexPage.tsx
@@ -2,12 +2,14 @@ import { useGetAllDisksQuery } from '../../store';
 import DiskItem from '../../components/DiskItem';
 import Skeleton from '../../components/Skeleton';
 import { useSnackbar } from 'notistack';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function DisksIndexPage() {
   const { enqueueSnackbar } = useSnackbar();
-  const { data, isLoading, isError, isSuccess } = useGetAllDisksQuery();
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const { data, isLoading, isError, isSuccess } = useGetAllDisksQuery('');
 
   useEffect(() => {
     if (!isLoading) {
@@ -19,6 +21,12 @@ export default function DisksIndexPage() {
       });
     }
   }, [enqueueSnackbar, isError, isLoading]);
+
+  const filteredDisks = isSuccess
+    ? data?.data?.filter((disk) =>
+        disk.name.toLowerCase().includes(searchQuery.toLowerCase())
+      )
+    : [];
 
   return (
     <div className="relative min-h-screen overflow-hidden">
@@ -43,20 +51,31 @@ export default function DisksIndexPage() {
         <h1 className="text-4xl font-bold mb-8 text-center text-white">
           All Disks ({data?.count || 0})
         </h1>
+        <div className="mb-3">
+          <input
+            type="search"
+            onChange={(e) => setSearchQuery(e.target.value)}
+            className="mx-auto relative m-0 block w-2/4 min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-200 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
+            id="search"
+            placeholder="Search..."
+          />
+        </div>{' '}
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
           {isLoading && <Skeleton className="h-72 w-full" times={6} />}
           {isError && <div className="text-red-600">Error Loading Disks!</div>}
           {isSuccess &&
-            (data?.message ? (
+            (filteredDisks.length === 0 ? (
               <div className="text-xl m-6 text-white">
-                The List is Empty at the Moment,{' '}
+                No disks found.{' '}
                 <Link to={'/users/login'} className="text-blue-700">
                   Login
                 </Link>{' '}
                 and start adding disks.
               </div>
             ) : (
-              data.data?.map((disk) => <DiskItem key={disk.id} disk={disk} />)
+              filteredDisks.map((disk) => (
+                <DiskItem key={disk.id} disk={disk} />
+              ))
             ))}
         </div>
       </div>
